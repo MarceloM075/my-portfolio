6ab3d0e6a4479823a717ae9d26610932eace42a1
Marcelo Marques Almeida <93165300+MarceloM075@users.noreply.github.com>
2023-03-22 17:18:50 -0300

    Adding Links component

---
+++ src/components/Links/Links.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+import './styles.css'
+
+function Links(props){
+    function isHome(){
+        if(!props.isHome){
+            return(
+                <ul>
+                    LOGO
+                </ul>
+            )
+        }
+    }
+
+    return(
+        <>
+            <nav className={ props.isHome ? 'links' : 'menu' } data-animation={ props.isHome ? '' : 'menu-animation' }>
+                {isHome()}
+                <ul>
+                    <li>
+                        <a className={ props.isHome ? 'links-itens' : 'nav-itens' } href="/">Projects</a>
+                    </li>
+                    <li>
+                        <a className={ props.isHome ? 'links-itens' : 'nav-itens' } href="/" disabled>Skills</a>
+                    </li>
+                    <li>
+                        <a className={ props.isHome ? 'links-itens' : 'nav-itens' } href="/">About Me</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default Links
