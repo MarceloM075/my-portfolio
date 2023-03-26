fb12d619b907e77bbfc619fbc0d3f8a6dd53460d
Marcelo Marques Almeida <93165300+MarceloM075@users.noreply.github.com>
2023-03-26 19:59:46 -0300

    Added the skills into a js archive

---
+++ src/assets/js/skills.js
@@ -0,0 +1,21 @@
+import reactIcon from "../../assets/img/react-svgrepo-com.svg"
+import cssIcon from "../../assets/img/css-svgrepo-com.svg"
+import htmlIcon from "../../assets/img/html-svgrepo-com.svg"
+import bootstrapIcon from "../../assets/img/bootstrap-svgrepo-com.svg"
+import gulpIcon from "../../assets/img/gulp-svgrepo-com.svg"
+import javascriptIcon from "../../assets/img/javascript-svgrepo-com.svg"
+import jqueryIcon from "../../assets/img/jquery-svgrepo-com.svg"
+import webpackIcon from "../../assets/img/webpack-svgrepo-com.svg"
+
+const SKILLS = {
+    'React': reactIcon,
+    'CSS': cssIcon,
+    'HTML': htmlIcon,
+    'Boostrap': bootstrapIcon,
+    'Gulp': gulpIcon,
+    'JavaScript': javascriptIcon,
+    'jQuery': jqueryIcon,
+    'Webpack': webpackIcon
+}
+
+export default SKILLS
